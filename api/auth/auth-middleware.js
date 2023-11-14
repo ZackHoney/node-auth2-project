e054de8a0301e7cb3fe84ddda5caf413c386ed7d
Zack Honey <zhoney466@gmail.com>
2023-11-14 12:13:42 -0800

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,5 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
-
+const { findBy } = require('../users/users-model')
 const restricted = (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header:
@@ -16,8 +16,8 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
- next()
-}
+    next()
+    }
 
 const only = role_name => (req, res, next) => {
   /*
@@ -34,7 +34,7 @@ const only = role_name => (req, res, next) => {
 }
 
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -42,8 +42,18 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
- next()
-}
+    try {
+      const [user] = await findBy({ username: req.body.username})
+      if (!user) {
+        next({ status: 422, message: 'Invalid credentials '})
+      } else {
+        req.user = user
+        next()
+      }
+    } catch (err) {
+      next(err)
+    }
+  }
 
 
 const validateRoleName = (req, res, next) => {
